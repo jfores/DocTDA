d65800788206cf4f9bb8e5c090b023dca6be0e80
jfores <fores.martos.jaume@gmail.com>
2022-01-20 11:27:03 +0100

    Close connections in Parsing_Vina

--- R/Parsing_Vina_Logs.R
+++ R/Parsing_Vina_Logs.R
@@ -12,7 +12,9 @@
 #' parse_max_energy_vina(log_file_path)
 #' }
 parse_max_energy_vina <- function(log_file_path){
-  read_temp <- readLines(log_file_path)
+  con <- file(log_file_path)
+  read_temp <- readLines(con)
   max_bin_en <- as.numeric(strsplit(read_temp[which(grepl("kcal/mol",read_temp)) + 2]," ")[[1]][13])
+  close(con)
   return(max_bin_en)
 }
